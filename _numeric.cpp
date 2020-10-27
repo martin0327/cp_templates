315ab2d6233290ed80f8cc1d149795e76d65d323
Martin Yang <martin.yang@lge.com>
2020-10-27 21:58:45 +0900

    update a cpp template for numeric problems

---
+++ _numeric.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define int long long
+
+using namespace std;
+
+///////////////////////////////////////////////////////////////////////
+
+
+signed main() {
+
+  cout.precision(17);
+  const double pi = 2 * acos(0.0);
+  
+  return 0;
+}
