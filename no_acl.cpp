3e89bffbf238fb0b9af2666078a099bcb85a73fc
Martin Yang <martin0327@gmail.com>
2023-09-06 00:56:57 +0900

    modified fenwick tree

--- no_acl.cpp
+++ no_acl.cpp
@@ -1149,35 +1149,35 @@ struct LCA {
 };
 
 template <class T> struct fenwick_tree {
-  public:
-    fenwick_tree() : _n(0) {}
-    fenwick_tree(int n) : _n(n), data(n) {}
+    int _n;
+    vector<T> d;
+
+    fenwick_tree(int n) {
+        this-n = n;
+        d.resize(n);
+    }
 
     void add(int p, T x) {
-        assert(0 <= p && p < _n);
+        assert(0 <= p && p < n);
         p++;
-        while (p <= _n) {
-            data[p - 1] += U(x);
+        while (p <= n) {
+            d[p - 1] += U(x);
             p += p & -p;
         }
     }
 
-    T sum(int l, int r) {
-        assert(0 <= l && l <= r && r <= _n);
-        return sum(r) - sum(l);
-    }
-
-  private:
-    int _n;
-    vector<T> data;
-
     T sum(int r) {
         T s = 0;
         while (r > 0) {
-            s += data[r - 1];
+            s += d[r - 1];
             r -= r & -r;
         }
         return s;
+    }
+
+    T sum(int l, int r) {
+        assert(0 <= l && l <= r && r <= n);
+        return sum(r) - sum(l);
     }
 };
 
