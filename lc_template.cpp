161f1ba57e51a7867d929b3399c8004790fe9c16
Martin Yang <martin0327@gmail.com>
2025-02-15 23:24:53 +0900

    modify lc_template

--- lc_template.cpp
+++ lc_template.cpp
@@ -1569,17 +1569,17 @@ int msb_pos(int x) {
     return ret;
 }
 
-template<typename T>
-void chmax(T &x, T y) {x = max(x,y);}
+// template<typename T>
+// void chmax(T &x, T y) {x = max(x,y);}
 
-template<typename T>
-void chmin(T &x, T y) {x = min(x,y);}
+// template<typename T>
+// void chmin(T &x, T y) {x = min(x,y);}
 
-template<typename T>
-void asort(vector<T> &a) {sort(a.begin(), a.end());}
+// template<typename T>
+// void asort(vector<T> &a) {sort(a.begin(), a.end());}
 
-template<typename T>
-void dsort(vector<T> &a) {sort(a.rbegin(), a.rend());}
+// template<typename T>
+// void dsort(vector<T> &a) {sort(a.rbegin(), a.rend());}
 
 template<typename T>
 void reverse(vector<T> &a) {reverse(a.begin(), a.end());}
@@ -1713,12 +1713,12 @@ set<T> get_set(vector<T> &a) {
     return ret;
 }
 
-template<typename T>
-vector<T> get_unique(vector<T> a) {
-    asort(a);
-    a.erase(unique(a.begin(), a.end()), a.end());
-    return a;
-}
+// template<typename T>
+// vector<T> get_unique(vector<T> a) {
+//     asort(a);
+//     a.erase(unique(a.begin(), a.end()), a.end());
+//     return a;
+// }
 
 template<typename T>
 vector<vector<T>> mat_id(int n) {
@@ -2008,6 +2008,31 @@ signed main() {
     return 0;
 }
 
+
+// template<typename T>
+// using min_pq = priority_queue<T, vector<T>, greater<T>>;
+// template<typename T>
+// using max_pq = priority_queue<T>;
+
+template<typename T>
+void chmax(T &x, T y) {x = max(x,y);}
+
+template<typename T>
+void chmin(T &x, T y) {x = min(x,y);}
+
+template<typename T>
+void asort(vector<T> &a) {sort(a.begin(), a.end());}
+
+template<typename T>
+void dsort(vector<T> &a) {sort(a.rbegin(), a.rend());}
+
+template<typename T>
+vector<T> get_unique(vector<T> a) {
+    asort(a);
+    a.erase(unique(a.begin(), a.end()), a.end());
+    return a;
+}
+
 using ll = long long;
 using vi = vector<ll>;
 using vvi = vector<vi>;
@@ -2017,14 +2042,10 @@ using ti3 = tuple<ll,ll,ll>;
 using vti3 = vector<ti3>;
 using vs = vector<string>;
 
-template<typename T>
-using min_pq = priority_queue<T, vector<T>, greater<T>>;
-template<typename T>
-using max_pq = priority_queue<T>;
 
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& a, int k) {
+    int maxDifference(string s) {
         
     }
 };
